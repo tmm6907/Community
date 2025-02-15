ba51abe7e38cd3dcacc947072c0d75becee3ea72
Terrence Moore <tmm6907@gmail.com>
2025-02-15 15:30:10 -0500

    Delete community/community directory

--- community/community/miner/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from miner.views import miner_view
-
-urlpatterns = [
-    path('miner/', miner_view, name="miner"),
-]
--- community/community/miner/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from miner.miner import search_nearby_place, zipcodes, places
-
-
-def miner_view(request):
-    for zipcode in zipcodes:
-        for place in places:
-            search_nearby_place(place, zipcode)
-    return HttpResponse('<h1> Success!</h1>')
